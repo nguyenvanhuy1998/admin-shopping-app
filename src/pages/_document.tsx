4bea546e69ac98449d3fe74b18336299189a975e
huy.nguyenvan <huy.nguyenvan@mafc.com.vn>
2024-08-01 11:09:40 +0700

    first commit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,55 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import React from "react";
+import { createCache, extractStyle, StyleProvider } from "@ant-design/cssinjs";
+import Document, { Head, Html, Main, NextScript } from "next/document";
+import type { DocumentContext } from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
+const MyDocument = () => (
+    <Html lang="vi">
+        <Head>
+            <title>Admin Shara App</title>
+            <link
+                rel="stylesheet"
+                href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+                integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+                crossOrigin="anonymous"
+            />
+            <link
+                rel="stylesheet"
+                href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+                integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+            />
+        </Head>
+        <body>
+            <Main />
+            <NextScript />
+        </body>
     </Html>
-  );
-}
+);
+
+MyDocument.getInitialProps = async (ctx: DocumentContext) => {
+    const cache = createCache();
+    const originalRenderPage = ctx.renderPage;
+    ctx.renderPage = () =>
+        originalRenderPage({
+            enhanceApp: (App) => (props) =>
+                (
+                    <StyleProvider cache={cache}>
+                        <App {...props} />
+                    </StyleProvider>
+                ),
+        });
+
+    const initialProps = await Document.getInitialProps(ctx);
+    const style = extractStyle(cache, true);
+    return {
+        ...initialProps,
+        styles: (
+            <>
+                {initialProps.styles}
+                <style dangerouslySetInnerHTML={{ __html: style }} />
+            </>
+        ),
+    };
+};
+
+export default MyDocument;
