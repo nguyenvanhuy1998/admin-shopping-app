60d8253d139efa6213cd7ab64fefe973e5e30da7
huy.nguyenvan <huy.nguyenvan@mafc.com.vn>
2024-08-01 14:13:39 +0700

    add firebase

---
+++ src/models/user.ts
@@ -0,0 +1,9 @@
+export interface User {
+    emailVerified: boolean;
+    creationTime: string;
+    uid: string;
+    displayName: string;
+    photoURL: any;
+    lastSignInTime: string;
+    email: string;
+}
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,7 +1,58 @@
-import React from "react";
+import { collectionNames } from "@/constants/collectionNames";
+import { fs } from "@/firebase/firebaseConfig";
+import { User } from "@/models/User";
+import { Button, Space } from "antd";
+import Table, { ColumnProps } from "antd/es/table";
+import { collection, onSnapshot } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { BiTrash } from "react-icons/bi";
 
 const Users = () => {
-    return <div>Users</div>;
+    const [users, setUsers] = useState<User[]>([]);
+    useEffect(() => {
+        onSnapshot(collection(fs, collectionNames.users), (snap) => {
+            if (snap.empty) {
+                console.log("Data not found");
+            } else {
+                const items: User[] = [];
+                snap.forEach((item: any) => {
+                    items.push(item.data());
+                });
+                setUsers(items);
+            }
+        });
+    }, []);
+    const columns: ColumnProps<User>[] = [
+        {
+            key: "name",
+            dataIndex: "displayName",
+            title: "Username",
+        },
+        {
+            key: "email",
+            dataIndex: "email",
+            title: "Email",
+        },
+        {
+            key: "createdAt",
+            dataIndex: "creationTime",
+            title: "Sign up",
+            render: (val: Date) => new Date(val).toLocaleString(),
+            align: "center",
+        },
+        {
+            key: "btn",
+            title: "",
+            dataIndex: "",
+            render: (item: User) => (
+                <Space>
+                    <Button icon={<BiTrash size={20} />} danger type="text" />
+                </Space>
+            ),
+            align: "right",
+        },
+    ];
+    return <Table dataSource={users} columns={columns} />;
 };
 
 export default Users;
